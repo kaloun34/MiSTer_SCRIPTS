c68375796a2e430360edcb5ad57ca23f7470213f
kaloun34 <54940754+kaloun34@users.noreply.github.com>
2024-11-07 13:55:36 +0100

    Add files via upload

---
+++ Verif_Mame_Merged.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+### Tonton ###
+# (07/11/2024)
+
+MIST="/media/fat"
+DIRM="/media/fat/Games/mame"
+url="https://myrient.erista.me/files/Internet%20Archive/chadmaster/mame-merged/mame-merged"
+SSL_SECURITY_OPTION="--insecure"
+CURL_RETRY="--connect-timeout 15 --max-time 180 --retry 3 --retry-delay 5 --show-error"
+
+
+# Renomme du core et parcours web
+search() {
+  for mame in "$DIRM"/*.zip; do
+    MAME=${mame##*/}
+    curl $CURL_RETRY $SSL_SECURITY_OPTION --fail --location --output /dev/null "$url/$MAME">/dev/null 2>&1
+    error_code=$?
+
+    if [ "$error_code" -eq 0 ]; then
+      echo "Le fichier $MAME existe !!"
+    else
+      echo "$MAME n'est a priori pas un merged" >> $MIST/MameMerged_NOK.txt
+    fi
+  done
+}
+
+
+#Début du script
+if [ -f $MIST/MameMerged_NOK.txt ]; then
+	rm $MIST/MameMerged_NOK.txt
+fi
+search

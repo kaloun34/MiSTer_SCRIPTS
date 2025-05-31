fc0c16297073824a3069724c33faf02db8586465
kaloun34 <54940754+kaloun34@users.noreply.github.com>
2025-05-31 13:02:13 +0200

    Optimisation Verif_MameToMra.sh
    - Remplacement de la recherche directe par un index unique (grep sur .zip dans tous les MRA)
    - Mise en readonly des variables critiques (répertoires, fichiers)
    - Ajout d’une fonction main() pour structurer le script
    - Nettoyage du fichier index.txt après traitement
    - Préparation d’un système de capture d’interruption via cleanup() (non activé pour l’instant)
    - Sortie des ROMs non trouvées dans MameToMra_NOK.txt

--- Verif_MameToMra.sh
+++ Verif_MameToMra.sh
@@ -6,47 +6,64 @@
 # Vérifie si les ROMS Mame du dossiers /Games/mame et sont bien utilisés par un MRA des dossiers /_Arcade et /_MiSTer++/_DualSDRAM/_Arcade
 
 
-MIST="/media/fat"
-DIRM="/media/fat/Games/mame"
-DIRHM="/media/fat/Games/hbmame"
-DIRA="/media/fat/_Arcade"
-DIRAD="/media/fat/_MiSTer++/_DualSDRAM/_Arcade"
+# Arrêt immédiat en cas d'erreur, etc....
+# FIXME: set -euo pipefail (ne fonctionne pas dans ce contexte, à creuser...)
 
+# Déclaration variables
+readonly MIST="/media/fat"
+readonly DIRM="/media/fat/Games/mame"
+readonly DIRHM="/media/fat/Games/hbmame"
+readonly DIRA="/media/fat/_Arcade"
+readonly DIRAD="/media/fat/_MiSTer++/_DualSDRAM/_Arcade"
+readonly TEMP="/media/fat/Scripts/.tonton"
+readonly OUT="MameToMra_NOK.txt"
+INDEX="$TEMP/index.txt"
+# --- Couleurs terminal ---
+readonly RED=$'\033[31m'
+readonly RESET=$'\033[0m'
 
-# Renomme du core
-renomme() {
-for mame in "$DIRM"/*.zip "$DIRHM"/*.zip; do
-    MAME="${mame##*/}"
-    MRASearch
+# FIXME: trap 'cleanup' INT TERM # Capture des signaux INT et TERM pour nettoyage propre (ne fonctionne pas)
 
-    if [ $? -ne 1 ]; then
-        echo "$mame NOK"
-        echo "$mame" >> "$MIST/Mame_NOK.txt"
+# === Fonction : main ===
+main() {
+    if [ -f "$MIST/$OUT" ]; then
+        rm "$MIST/$OUT"
     fi
-    done
+
+    printf "Ces ROMS ne semblent pas être utilisés par un MRA Arcade:\n">"$MIST/$OUT"
+    printf ".\n"
+    printf "Verification des ROMS en cours ....\n"
+    printf ".\n"
+    index_MRA
+    MRASearch
+    rm -f "$INDEX"
+}
+
+# Création d'un fichier index
+# il est intuile de reparcourir tous les MRAs à chaque occurence de $zip
+index_MRA() {
+    printf "Extraction brute des lignes contenant .zip dans les MRA...\n"
+    find "$DIRA" "$DIRAD" -type f -name "*.mra" -print0 | xargs -0 grep -i '\.zip' > "$INDEX"
 }
 
 # Parcours des MRA
 MRASearch () {
-shopt -s nullglob globstar
-for MRA in "$DIRA"/**/*.mra "$DIRAD"/**/*.mra ;do
-	if grep -qi "$MAME" "$MRA" ;then
-		return 1
-	fi
-done
+    for zip in "$DIRM"/*.zip "$DIRHM"/*.zip; do
+        rom="${zip##*/}"
 
-shopt -u nullglob globstar
-return 0
+        if ! grep -Fq "$rom" "$INDEX"; then
+            printf "%s NOK\n" "$rom"
+            printf "%s\n" "$rom" >> "$MIST/$OUT"
+        fi
+    done
 }
 
-#Début du script
-if [ -f "$MIST"/Mame_NOK.txt ]; then
-	rm "$MIST"/Mame_NOK.txt
-fi
+# === Fonction : cleanup ===
+# Non fonctionnelle pour le moment
+cleanup() {
+    printf "\n%sInterruption détectée. Nettoyage...%s\n" "$RED" "$RESET" >&2
+    #exit 1
+	return 1
+}
 
-echo "Ces ROMS ne semblent pas être utilisés par un MRA Arcade:">"$MIST"/Mame_NOK.txt
-echo "">>"$MIST"/Mame_NOK.txt
-echo .
-echo "Verification des ROMS en cours ...."
-echo .
-renomme
+main "$@"

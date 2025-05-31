03a70bdfe77b1efeaafd3fc3a0232a41e2da02f1
kaloun34 <54940754+kaloun34@users.noreply.github.com>
2025-05-31 12:57:44 +0200

    Amélioration script Verif_MameToMameMerged.sh
    - Ajout d'une fonction main() pour meilleure lisibilité
    - Mise en readonly des variables critiques (répertoires, URL, options curl)
    - Nettoyage conditionnel du fichier de sortie MameToMameMergedOnline_NOK.txt
    - Affichage dynamique des noms de roms trouvées (avec overwrite de ligne)
    - Préparation d'une gestion des interruptions avec trap/cleanup (à finaliser)
    - Remplacement de echo par printf pour plus de robustesse

--- Verif_MameToMameMergedSite.sh
+++ Verif_MameToMameMergedSite.sh
@@ -3,31 +3,55 @@
 ### Tonton ###
 # (07/11/2024)
 
-MIST="/media/fat"
-DIRM="/media/fat/Games/mame"
-url="https://myrient.erista.me/files/Internet%20Archive/chadmaster/mame-merged/mame-merged"
-SSL_SECURITY_OPTION="--insecure"
-CURL_RETRY="--connect-timeout 15 --max-time 180 --retry 3 --retry-delay 5 --show-error"
 
+# Arrêt immédiat en cas d'erreur, etc....
+# FIXME: set -euo pipefail (ne fonctionne pas dans ce contexte, à creuser...)
+
+# Déclaration variables
+readonly MIST="/media/fat"
+readonly DIRM="/media/fat/Games/mame"
+readonly OUT="MameToMameMergedOnline_NOK.txt"
+# Variables pour wget ou curl
+readonly URL="https://myrient.erista.me/files/Internet%20Archive/chadmaster/mame-merged/mame-merged"
+readonly SSL_SECURITY_OPTION="--insecure"
+readonly CURL_RETRY="--connect-timeout 15 --max-time 180 --retry 3 --retry-delay 5 --show-error"
+# --- Couleurs terminal ---
+readonly RED=$'\033[31m'
+readonly RESET=$'\033[0m'
+
+# FIXME: trap 'cleanup' INT TERM # Capture des signaux INT et TERM pour nettoyage propre (ne fonctionne pas)
+
+
+# === Fonction : main ===
+main() {
+	if [ -f $MIST/"$OUT" ]; then
+    rm $MIST/"$OUT"
+	fi
+
+  search
+}
 
 # Renomme du core et parcours web
 search() {
   for mame in "$DIRM"/*.zip; do
-    MAME=${mame##*/}
-    curl $CURL_RETRY $SSL_SECURITY_OPTION --fail --location --output /dev/null "$url/$MAME">/dev/null 2>&1
+    mame=${mame##*/}
+    curl $CURL_RETRY $SSL_SECURITY_OPTION --fail --location --output /dev/null "$URL/$mame">/dev/null 2>&1
     error_code=$?
 
     if [ "$error_code" -eq 0 ]; then
-      echo "$MAME est une rom merged"
+      printf "%s est une rom merged          \r" "$mame"
     else
-      echo "$MAME n'est a priori pas une rom merged" >> $MIST/MameMerged_NOK.txt
+      printf "%s n'est a priori pas une rom merged\n" "$mame" >> $MIST/MameMerged_NOK.txt
     fi
   done
 }
 
+# === Fonction : cleanup ===
+# Non fonctionnelle pour le moment
+cleanup() {
+    printf "\n%sInterruption détectée. Nettoyage...%s\n" "$RED" "$RESET" >&2
+    #exit 1
+	return 1
+}
 
-#Début du script
-if [ -f $MIST/MameMerged_NOK.txt ]; then
-	rm $MIST/MameMerged_NOK.txt
-fi
-search
+main "$@"
